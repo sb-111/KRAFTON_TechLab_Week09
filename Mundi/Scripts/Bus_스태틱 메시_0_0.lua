2e8f71ea939fe86423d3a980d93d2bae5a7f9ac7
김민찬 <mcminchan1021@gmail.com>
2025-11-04 03:04:26 +0900

    feat: 씬 로드 세이브 정상화, 회전문 생성

--- Mundi/Scripts/Bus_스태틱 메시_0_0.lua
+++ Mundi/Scripts/Bus_스태틱 메시_0_0.lua
@@ -67,6 +67,7 @@ end
 
 function BeginOverlap(Other)
     Velocity = Velocity * (-1.0)
+
     obj:SetActorLocation(obj:GetActorLocation() + Velocity:GetNormalized())
     UI:SetAfterCollisionTime(3)
     UI:AddScore(-30)
