ff8701e79917e407c180a500161f712479687bbd
김민찬 <mcminchan1021@gmail.com>
2025-11-04 04:28:04 +0900

    feat: 씬 정적 장애물 추가, 회전 장애물 스크립트 추가, 기존 플레이어 충돌 로직 수정(다시 속도 기준으로. 위치기반은 정적 장애물과 안 맞음)

--- Mundi/Scripts/Bus_스태틱 메시_0_0.lua
+++ Mundi/Scripts/Bus_스태틱 메시_0_0.lua
@@ -74,7 +74,7 @@ function BeginOverlap(Other)
     Velocity = Velocity * (-1.0)
     
 
-    obj:SetActorLocation(obj:GetActorLocation() + obj:GetActorRight())
+    obj:SetActorLocation(obj:GetActorLocation() + Velocity:GetNormalized())
     UI:SetAfterCollisionTime(3)
     UI:AddScore(-30)
 
