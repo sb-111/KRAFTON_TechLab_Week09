e7db3be5e90dc3993c78d7079b8cf90a0f60f418
김민찬 <mcminchan1021@gmail.com>
2025-11-04 03:12:10 +0900

    fix: 충돌시 현재 속도가 아니라 현재 플레이어가 바라보는 방향 반대로 밀어서 낑김현상 해결

--- Mundi/Scripts/Bus_스태틱 메시_0_0.lua
+++ Mundi/Scripts/Bus_스태틱 메시_0_0.lua
@@ -67,8 +67,9 @@ end
 
 function BeginOverlap(Other)
     Velocity = Velocity * (-1.0)
+    
 
-    obj:SetActorLocation(obj:GetActorLocation() + Velocity:GetNormalized())
+    obj:SetActorLocation(obj:GetActorLocation() + obj:GetActorRight())
     UI:SetAfterCollisionTime(3)
     UI:AddScore(-30)
 end
