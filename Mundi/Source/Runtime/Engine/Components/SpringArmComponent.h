703712c37234a4232ce6bea465181ad7917c308f
김민찬 <mcminchan1021@gmail.com>
2025-11-06 02:49:39 +0900

    feat: Spring Arm에 bUseControllerRotation 추가, 마우스를 통한 Controller의 회전 정보를 사용해서 Camera를 Arm의 자식에 부착하면 Spring Arm과 같이 회전하도록 함. Spring Arm이 없으면 마우스를 움직여도 회전하지 않음

--- Mundi/Source/Runtime/Engine/Components/SpringArmComponent.h
+++ Mundi/Source/Runtime/Engine/Components/SpringArmComponent.h
@@ -10,6 +10,7 @@ public:
 	USpringArmComponent();
 
 	void OnRegister(UWorld* InWorld) override;
+	void BeginPlay() override;
 
 	FTransform GetSocketWorldTransform() const override;
 	void TickComponent(float DeltaTime) override;
@@ -31,4 +32,7 @@ private:
 	FVector SocketOffset = { 0.0f, 0.0f, 0.0f }; // 암 끝 기준 위치 오프셋
 	FVector TargetOffset = { 0.0f, 0.0f, 0.0f }; // "무엇을 볼지" 피벗 보정(부모 좌표계 기준)
 	float BackoffEpsilon = 0.1f;                // 히트 시 살짝 당겨서 떨림 방지
+
+	FQuat InitialRotation;
+	bool bUseControllerRotation = true;		// 컨트롤러의 Rotation 사용
 };
--- Mundi/Source/Runtime/Engine/GameFramework/PlayerController.h
+++ Mundi/Source/Runtime/Engine/GameFramework/PlayerController.h
@@ -17,13 +17,20 @@ public:
 	void Tick(float DeltaSecond) override;
 
 	APlayerCameraManager* GetPlayerCameraManager();
+	const FQuat& GetControlRotation() { return ControlRotation; }
 
 	void ProcessInput();
+	void ProcessMouseInput();
 
 private:
 
 	// 빙의 대상
 	TWeakPtr<APawn> Pawn;
+
+	FQuat ControlRotation;
+	float MouseSensitivity = 0.1f;  // 기존 World에서 사용하던 값으로 조정
+	float CameraYawDeg = 0.0f;   
+	float CameraPitchDeg = 0.0f; 
 
 	APlayerCameraManager* PlayerCameraManager = nullptr;
 };
